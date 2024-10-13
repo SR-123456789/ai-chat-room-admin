b4ee0f32e0284508681112a4894b7efbc499ca68
soshi1234 <148639191+soshi1234@users.noreply.github.com>
2024-10-13 18:37:48 +0900

    fixup! build設定

---
+++ out/_next/static/chunks/app/layout-ba85e33ff5957e95.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[185],{1538:function(n,e,t){Promise.resolve().then(t.t.bind(t,5878,23)),Promise.resolve().then(t.t.bind(t,2778,23))},2778:function(){}},function(n){n.O(0,[461,878,971,117,744],function(){return n(n.s=1538)}),_N_E=n.O()}]);
